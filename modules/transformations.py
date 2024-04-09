7c8af9544a6852df63f5fd9db49386e356049dda
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2024-04-09 18:11:55 +0100

    bug check

--- modules/transformations.py
+++ modules/transformations.py
@@ -196,7 +196,9 @@ class GPUTransformNeuralfp(nn.Module):
                 x_j = self.val_transform(x_j.view(1,1,x_j.shape[-1]), sample_rate=self.sample_rate)
 
             X_j = self.logmelspec(x_j.squeeze(0)).transpose(1,0)
+            print(f"Intermediate X_j shape {X_j.shape}")
             X_j = X_j.unfold(0, size=self.n_frames, step=int(self.n_frames*(1-self.overlap)))
+            print(f"After unfold X_j shape {X_j.shape}")
             X_j = self.extractor(X_j)
 
         return X_i, X_j
