0c9cc74cc7d4917e70be8acb6d0f4796c18a6a51
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2024-04-16 18:21:11 +0100

    reverse snr list

--- test_pipeline.sh
+++ test_pipeline.sh
@@ -1,7 +1,7 @@
 
 # Add a loop to run test with a list of test_snr
 
-for snr in 0 5 10 20
+for snr in 20 10 5 0
 do
     echo "Test with SNR = $snr"
     python test_fp.py --query_lens=1,2,3,5 --test_snr=$snr --label=snr_$snr
