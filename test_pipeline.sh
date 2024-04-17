630cc9f44c191b70124d8993b180b70ec36fddb3
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2024-04-17 01:11:15 +0100

    retracting to older config

--- test_pipeline.sh
+++ test_pipeline.sh
@@ -4,5 +4,5 @@
 for snr in 20 10 5 0
 do
     echo "Test with SNR = $snr"
-    python test_fp.py --query_lens=1,2,3,5 --n_dummy_db=500 --test_snr=$snr --label=sz_500_snr_$snr
+    python test_fp.py --query_lens=1,2,3,5 --n_dummy_db=2000 --test_snr=$snr --label=sz_500_snr_$snr
 done
