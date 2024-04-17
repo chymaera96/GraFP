e67e2cc714b3762de4aca17f9e10ba187c0a00f5
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2024-04-17 09:52:31 +0100

    labelling error fix

--- test_pipeline.sh
+++ test_pipeline.sh
@@ -5,5 +5,5 @@ for snr in 20 10 5 0
 do
     echo "Test with SNR = $snr"
     top -b -n 1 -u ab017| grep python | awk '{print $1}' | xargs kill
-    python test_fp.py --query_lens=1,2,3,5 --n_dummy_db=500 --test_snr=$snr --label=sz_500_snr_$snr
+    python test_fp.py --query_lens=1,2,3,5 --n_dummy_db=500 --test_snr=$snr --text=sz_500_snr_$snr
 done
