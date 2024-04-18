2699f374f2136ccb68e3ec4a052c12c4ec339f9f
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2024-04-18 09:51:56 +0100

    checking fully connected projector

--- simclr/simclr.py
+++ simclr/simclr.py
@@ -15,16 +15,21 @@ class SimCLR(nn.Module):
         h = cfg['h']
         u = cfg['u']
 
-        self.projector = nn.Sequential(nn.Conv1d(h, d * u, kernel_size=(1,), groups=d),
-                                        nn.ELU(),
-                                        nn.Conv1d(d * u, d, kernel_size=(1,), groups=d)
-                                        )
+        self.projector = nn.Sequential(nn.Linear(h, d*u),
+                                       nn.ELU(),
+                                       nn.Linear(d*u, d)
+                               )
+
+        # self.projector = nn.Sequential(nn.Conv1d(h, d * u, kernel_size=(1,), groups=d),
+        #                                 nn.ELU(),
+        #                                 nn.Conv1d(d * u, d, kernel_size=(1,), groups=d)
+        #                                 )
 
     def forward(self, x_i, x_j):
         
 
         h_i = self.encoder(x_i)
-        # print(f'Shape of h_i {h_i.shape} inside the SimCLR forward function')
+        print(f'Shape of h_i {h_i.shape} inside the SimCLR forward function')
         z_i = self.projector(h_i.unsqueeze(-1)).squeeze(-1)
         # print(f'Shape of z_i {z_i.shape} inside the SimCLR forward function')
         z_i = F.normalize(z_i, p=2)
