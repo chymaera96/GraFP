85ca322394489e747affb750af21253e7283bf43
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2024-05-02 18:23:40 +0100

    bug check

--- train.py
+++ train.py
@@ -66,7 +66,12 @@ def train(cfg, train_loader, model, optimizer, ir_idx, noise_idx, augment=None):
         assert x_i.device == torch.device('cuda:0'), f"[IN TRAINING] x_i device: {x_i.device}"
         l1_i, l1_j, z_i, z_j = model(x_i, x_j)
 
-        loss = ntxent_loss(z_i, z_j, cfg) + cfg['lambda'] * (l1_i + l1_j)
+
+        loss = ntxent_loss(z_i, z_j, cfg) #+ cfg['lambda'] * (l1_i + l1_j)
+        l1_loss = cfg['lambda'] * (l1_i + l1_j)
+        print(loss.shape)
+        print(l1_loss.shape)
+        return
         loss.backward()
         optimizer.step()
 
