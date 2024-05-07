86aab95f7eb34cf99872d5459523df311cb34dbf
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2024-05-07 23:50:06 +0100

    pc18

--- peak_extractor.py
+++ peak_extractor.py
@@ -414,7 +414,7 @@ class GPUPeakExtractorv2(nn.Module):
         peaks = self.peak_from_features(spec_tensor.unsqueeze(1))
         feature = self.conv(peaks)
         self.l1 = torch.norm(feature, p=1)
-        peaks = self.peak_from_features(feature, as_mask=True)
+        peaks = self.peak_from_features(feature, as_mask=False)
 
 
         T_tensor = torch.arange(feature.shape[3], device=feature.device) / feature.shape[3]
