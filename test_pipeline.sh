e1f707f61283a7cfd34806c24c5383fe7d2278e7
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2024-05-15 16:16:59 +0100

    recomputing less for optimization

--- test_pipeline.sh
+++ test_pipeline.sh
@@ -1,9 +1,13 @@
 
 # Add a loop to run test with a list of test_snr
+# Take user as parameter
+
+# Usage: bash test_pipeline.sh ab017
+
 
 for snr in 20 10 5
 do
     echo "Test with SNR = $snr"
-    top -b -n 1 -u acw723 | grep python | awk '{print $1}' | xargs kill -9
+    top -b -n 1 -u $1 | grep python | awk '{print $1}' | xargs kill -9
     python test_fp.py --query_lens=1,2,3,5 --n_dummy_db=100 --test_snr=$snr --text=sanity_test_$snr --small_test=False
 done
