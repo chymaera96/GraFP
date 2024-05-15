1e5bf843024a4435290ae5ce98bb7234f543877e
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2024-05-15 17:52:19 +0100

    testing small_test flag

--- test_pipeline.sh
+++ test_pipeline.sh
@@ -5,9 +5,11 @@
 # Usage: bash test_pipeline.sh ab017
 
 
-for snr in 20 10 5
+for snr in 20 10 5 0
 do
     echo "Test with SNR = $snr"
     top -b -n 1 -u $1 | grep python | awk '{print $1}' | xargs kill -9
-    python test_fp.py --query_lens=1,2,3,5 --n_dummy_db=100 --test_snr=$snr --text=sanity_test_$snr --small_test=False
+    # python test_fp.py --query_lens=1,2,3,5 --n_dummy_db=100 --test_snr=$snr --text=fma_medium_$snr --small_test=False
+    python test_fp.py --query_lens=1,2,3,5 --test_snr=$snr --text=fma_medium_$snr --small_test=False
+
 done
