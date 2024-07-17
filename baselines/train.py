126986c181c862b506e4ffa8cbaaab45739f9400
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2024-07-17 21:01:10 +0100

    bug check

--- baselines/train.py
+++ baselines/train.py
@@ -64,7 +64,7 @@ def train(cfg, train_loader, model, optimizer, scaler, ir_idx, noise_idx, augmen
         optimizer.zero_grad()
         x_i = x_i.to(device)
         x_j = x_j.to(device)
-
+        print(x_i.shape, x_j.shape)
         # with torch.autocast(device_type='cuda', dtype=torch.float16, enabled=True):
         with torch.no_grad():
             x_i, x_j = augment(x_i, x_j)
