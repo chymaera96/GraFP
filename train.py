156164c6df53c9f38f38a4d3e3cf4b4518aa6573
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2024-07-22 13:50:12 +0100

    minor fix

--- train.py
+++ train.py
@@ -166,7 +166,7 @@ def main():
         # TODO: Add support for resnet encoder (deprecated)
         raise NotImplementedError
     elif args.encoder == 'grafp':
-        model = SimCLR(cfg, encoder=GraphEncoder(cfg=cfg, in_channels=cfg['n_filters']), k=args.k)
+        model = SimCLR(cfg, encoder=GraphEncoder(cfg=cfg, in_channels=cfg['n_filters'], k=args.k))
         if torch.cuda.device_count() > 1:
             print("Using", torch.cuda.device_count(), "GPUs!")
             # model = DataParallel(model).to(device)
