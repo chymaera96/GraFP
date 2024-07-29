639d60fd94bb2f60cafb54b38dec4018e48e56ec
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2024-07-29 10:04:02 +0100

    eval setup for tck

--- test_pipeline.sh
+++ test_pipeline.sh
@@ -1,11 +1,12 @@
 # Usage: bash test_pipeline.sh ab017
 
 
-for snr in 20 15 10 5 0
+# for snr in 20 15 10 5 0
+for snr in 0
 do
     echo "Test with SNR = $snr"
     # top -b -n 1 -u $1 | grep python | awk '{print $1}' | xargs kill -9
     # python test_fp.py --query_lens=1,2,3,5 --n_dummy_db=100 --test_snr=$snr --text=fma_medium_$snr --small_test=False
-    python test_fp.py --query_lens=1,2,3,5 --n_query_db=350 --test_snr=$snr --text=350_fma_medium_$snr
+    python test_fp.py --query_lens=1 --n_query_db=350 --test_snr=$snr --text=350_fma_medium_$snr
 
 done
