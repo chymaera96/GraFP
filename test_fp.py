bcf701a86695a99ebc32502dbf38d1a3a6590ce0
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2024-08-25 00:12:58 +0100

    grafp largeval setup bug check

--- test_fp.py
+++ test_fp.py
@@ -134,7 +134,7 @@ def create_dummy_db(dataloader, augment, model, output_root_dir, fname='dummy_db
         assert x_i.size(1) == 64 and len(x_i.size()) == 3, f"Shape of x_i: {x_i.shape}"
         if x_i.size(0) < 2:
             x_list = [x_i]
-        else:
+        elif x_i.size(0) > 256:
             x_1, x_2 = torch.chunk(x_i, 2, dim=0)
             x_list = [x_1, x_2]
         for x_i in x_list:
