766445f3eefded733060bbc47aa26d055badd892
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2024-08-25 02:24:22 +0100

    conflict management; new bash file

---
+++ baselines/ablation.sh
@@ -0,0 +1,8 @@
+
+for snr in 20 15 10 5 0
+# for snr in 0
+do
+    echo "Test with SNR = $snr"
+    python test_fp.py --query_lens=1,2,3,5 --n_query_db=500 --test_snr=$snr --text=1000_fma_medium_$snr --test_ids=4000 --model=$1
+
+done
