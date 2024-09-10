647b6983eee5edd1bd78c7819e242638ca09965c
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2024-09-10 03:38:55 +0100

    sanir reprod setup

--- test_pipeline.sh
+++ test_pipeline.sh
@@ -6,7 +6,7 @@ do
     echo "Test with SNR = $snr"
     # top -b -n 1 -u $1 | grep python | awk '{print $1}' | xargs kill -9
     # python test_fp.py --query_lens=1,2,3,5 --n_dummy_db=100 --test_snr=$snr --text=fma_medium_$snr --small_test=False
-    python test_fp.py --query_lens=1,2,3,5 --n_query_db=500 --test_snr=$snr --text=4000_fma_medium_$snr --test_ids=logs/store/test/model_tc_29_best/test_ids.npy --model=$1
+    python test_fp.py --query_lens=1,2,3,5 --n_query_db=500 --test_snr=$snr --text=sanir_fma_medium_$snr --test_ids=logs/store/test/model_tc_29_best/test_ids.npy --model=$1
     # python test_fp.py --query_lens=1,2,3,5 --n_query_db=500 --test_snr=$snr --text=4000_fma_large_$snr --test_ids=4000 --test_dir=/data/scratch/acw723/fma_large --model=$1
 
 
