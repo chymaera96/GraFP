c1674e33c702b0bab685ed07ba4b41a9f2dc3f7b
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2024-09-10 03:11:33 +0100

    reprod largeval (tc10)

--- baselines/test_pipeline.sh
+++ baselines/test_pipeline.sh
@@ -1,10 +1,12 @@
 
-# for snr in 20 15 10 5 0
-for snr in 0
+for snr in 20 15 10 5 0
+# for snr in 0
 do
     echo "Test with SNR = $snr"
     # python test_fp.py --query_lens=1,2,3,5 --n_query_db=500 --test_snr=$snr --text=1000_fma_medium_$snr --test_ids=4000
-    python test_fp.py --query_lens=1,2,3,5 --n_query_db=500 --test_snr=$snr --text=1000_fma_large_$snr --test_ids=4000 --test_dir=../data/fma_large.json --model=$1
+    python test_fp.py --query_lens=1,2,3,5 --n_query_db=500 --test_snr=$snr --text=4000_fma_large_$snr \
+    --test_ids=/data/scratch/acw723/logs/store/test/model_tc_29_best/test_ids.npy \
+    --test_dir=../data/fma_large.json --model=$1
 
 
 done
