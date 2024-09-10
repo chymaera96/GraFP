0d7855715e806390369da5cced2589299aeaf725
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2024-09-10 21:12:41 +0100

    reprod largeval snr=5 (tc29)

--- ablation.sh
+++ ablation.sh
@@ -8,8 +8,8 @@
 # done
 
 
-for snr in 20 15 10 5
-# for snr in 0
+# for snr in 20 15 10 5
+for snr in 5
 do
     echo "Test with SNR = $snr"
     python test_fp.py --query_lens=1,2,3,5 --n_query_db=500 --test_snr=$snr \
