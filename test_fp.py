c994f81cb2e0db024227aa4187c1f9c52f7b1be4
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2024-09-11 15:08:32 +0100

    reprod largeval snr=5 with computed query

--- test_fp.py
+++ test_fp.py
@@ -317,8 +317,8 @@ def main():
             else:
                 print("=> Skipping dummy db creation...")
 
-            create_fp_db(query_db_loader, augment=test_augment, 
-                         model=model, output_root_dir=fp_dir, verbose=False)
+            # create_fp_db(query_db_loader, augment=test_augment, 
+            #              model=model, output_root_dir=fp_dir, verbose=False)
             
             
             text = f'{args.text}_{str(epoch)}'
