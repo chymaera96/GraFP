19f8caa492c09ae409ae6e552bd5e31a81cfd51a
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2024-10-03 00:29:36 +0100

    testing index cleanup

--- util.py
+++ util.py
@@ -18,65 +18,6 @@ class DummyScaler:
     def update(self):
         pass
 
-
-# def load_index(cfg, data_dir, ext=['wav','mp3'], mode="train"):
-
-#     if data_dir.endswith('.json'):
-#         print(f"=>Loading indices from index file {data_dir}")
-#         with open(data_dir, 'r') as fp:
-#             dataset = json.load(fp)
-#         return dataset
-    
-#     print(f"=>Loading indices from {data_dir}")
-#     train_json_path = os.path.join('data', data_dir.split('/')[-1] + "_train.json")
-#     valid_json_path = os.path.join('data', data_dir.split('/')[-1] + "_valid.json")
-
-#     if mode == "train" and os.path.exists(train_json_path):
-#         print(f"Train index exists. Loading indices from {train_json_path}")
-#         with open(train_json_path, 'r') as fp:
-#             train = json.load(fp)
-    
-#     elif mode != "train" and os.path.exists(valid_json_path):
-#         print(f"Valid index exists. Loading indices from {valid_json_path}")
-#         with open(valid_json_path, 'r') as fp:
-#             valid = json.load(fp)
-
-#     else:
-#         print(f"Creating new index files {train_json_path} and {valid_json_path}")
-#         train_len = cfg['train_sz']
-#         valid_len = cfg['val_sz']
-#         idx = 0
-#         train = {}
-#         for fpath in glob.iglob(os.path.join(data_dir,'**/*.*'), recursive=True):
-#             if len(train) >= train_len:
-#                 break
-#             if fpath.split('.')[-1] in ext: 
-#                 train[str(idx)] = fpath
-#                 idx += 1
-
-#         with open(train_json_path, 'w') as fp:
-#             json.dump(train, fp)
-
-#         idx = 0
-#         valid = {}
-#         existing = [f.split('/')[-1] for f in list(train.values())]
-#         for fpath in glob.iglob(os.path.join(data_dir,'**/*.*'), recursive=True):
-#             if len(valid) >= valid_len:
-#                 break
-#             if fpath.split('.')[-1] in ext and fpath.split('/')[-1] not in existing:
-#                 valid[str(idx)] = fpath
-#                 idx += 1
-
-#         with open(valid_json_path, 'w') as fp:
-#             json.dump(valid, fp)
-
-#     if mode == "train":
-#         dataset = train
-#     else:
-#         dataset = valid
-
-#     return dataset
-
 def load_index(cfg, data_dir, ext=['wav','mp3'], shuffle_dataset=True, mode="train"):
 
     if data_dir.endswith('.json'):
@@ -169,10 +110,6 @@ def seconds_from_query_len(query_len, overlap, dur):
 
 def load_ckp(checkpoint_fpath, model, optimizer, scheduler):
     checkpoint = torch.load(checkpoint_fpath)
-    # # Check if dataparallel is used
-    # if 'module' in list(checkpoint['state_dict'].keys())[0]:
-    #     print("Loading model with dataparallel...")
-    #     checkpoint = {key.replace('module.', ''): value for key, value in checkpoint.items()}
     model.load_state_dict(checkpoint['state_dict'])
     optimizer.load_state_dict(checkpoint['optimizer'])
     scheduler.load_state_dict(checkpoint['scheduler'])
@@ -192,26 +129,6 @@ def load_config(config_path):
 def override(config_val, arg):
     return arg if arg is not None else config_val 
 
-# def create_fp_dir(resume=None, ckp=None, epoch=1, train=True, large=False):
-
-#     if train:
-#         parent_dir = 'logs/emb/valid'
-#     else:
-#         if large:
-#             parent_dir = '/data/scratch/acw723/logs/emb/test'
-#         else:
-#             parent_dir = 'logs/emb/test'
-
-#     if not os.path.exists(parent_dir):
-#         os.makedirs(parent_dir)
-#     if resume is not None:
-#         ckp_name = resume.split('/')[-1].split('.pt')[0]
-#     else:
-#         ckp_name = f'model_{ckp}_epoch_{epoch}'
-#     fp_dir = os.path.join(parent_dir, ckp_name)
-#     if not os.path.exists(fp_dir):
-#         os.mkdir(fp_dir)
-#     return fp_dir
 
 def create_fp_dir(resume=None, ckp=None, epoch=1, train=True, large=False):
 
@@ -234,47 +151,17 @@ def create_fp_dir(resume=None, ckp=None, epoch=1, train=True, large=False):
         os.mkdir(fp_dir)
     return fp_dir
 
+def update_index(data_dir, idx_path, ext=['wav','mp3']):
+    # Update paths with new parent directory
+    new_index = {}
+    with open(idx_path, 'r') as fp:
+        index = json.load(fp)
+    dir_name = idx_path.split('/')[-1].split('.')[0]
+    for key, value in index.items():
+        rel_path = value.split(dir_name)[-1]
+        new_index[key] = os.path.join(data_dir, dir_name, rel_path)
 
-
-def create_train_set(data_dir, dest, size=10000):
-    if not os.path.exists(dest):
-        os.mkdir(dest)
-        print(data_dir)
-        print(dest)
-    for ix,fname in enumerate(os.listdir(data_dir)):
-        fpath = os.path.join(data_dir, fname)
-        if ix <= size and fpath.endswith('mp3'):
-            shutil.move(fpath,dest)
-            print(ix)
-        if len(os.listdir(dest)) >= size:
-            return dest
-    
-    return dest
-
-def create_downstream_set(data_dir, size=5000):
-    src = os.path.join(data_dir, f'fma_downstream')
-    dest = data_dir
-    # if not os.path.exists(dest):
-    #     os.mkdir(dest)   
-    # if len(os.listdir(dest)) >= size:
-    #     return dest
-    for ix,fname in enumerate(os.listdir(src)):
-        fpath = os.path.join(src, fname)
-        if not fpath.endswith('mp3'):
-            continue
-        # if ix < size:
-        if len(os.listdir(src)) > 500:
-            shutil.move(fpath,dest)
-
-    return dest
-
-def preprocess_aug_set_sr(data_dir, sr=22050):
-    for fpath in glob.iglob(os.path.join(data_dir,'**/*.wav'), recursive=True):
-        y, sr = sf.read(fpath)
-        print(sr)
-        break
-        # sf.write(fpath, data=y, samplerate=sr)
-    return
+    return new_index
 
 def count_parameters(model, encoder):
     table = PrettyTable(["Modules", "Parameters"])
@@ -298,33 +185,4 @@ def calculate_output_sparsity(output):
 
     sparsity = zero_elements / total_elements * 100
     return sparsity
-
-    # Get paths of files not in the index
-def get_test_index(data_dir):
-    train_idx = load_index(data_dir)
-    all_file_list = glob.glob(os.path.join(data_dir,'**/*.mp3'), recursive=True)
-    print(f'Number of files in {data_dir}: {len(all_file_list)}')
-    # test_idx = {str(i):f for i,f in enumerate(all_file_list) if f not in train_idx.values()}
-    idx = 0
-    test_idx = {}
-    for i, fpath in enumerate(all_file_list):
-        if i % 200 == 0:
-            print(f"Processed {i}/{len(all_file_list)} files")
-        if fpath not in train_idx.values():
-            test_idx[str(idx)] = fpath
-            idx += 1
-
-    return test_idx
     
-def main():
-
-    path = '/data/scratch/acw723/fma_large'
-    cfg = load_config('config/grafp.yaml')
-    data = load_index(cfg, path)
-    # Check if all paths are valid
-    for idx, fpath in data.items():
-        if not os.path.exists(fpath):
-            print(f"File {fpath} not found")
-    
-if __name__ == '__main__':
-    main()
