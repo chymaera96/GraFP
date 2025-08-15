5ca473321a54c0425db284d5294fb72192ef1c1c
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2025-08-15 19:12:12 +0100

    bug check

--- util.py
+++ util.py
@@ -171,7 +171,7 @@ def update_index(data_dir, idx_path):
                 new_index[key].append(os.path.join(data_dir, rel_path))
     else:
         for key, value in index.items():
-            rel_path = value.split(dir_name)[-1][1:]
+            rel_path = value.split(f"{dir_name}/")[-1][1:]
             new_index[key] = os.path.join(data_dir, rel_path)
 
     with open(idx_path, 'w') as fp:
